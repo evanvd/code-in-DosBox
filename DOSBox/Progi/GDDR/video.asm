afcbbc73ecc469fadd0b47f67c2e0aaf81708731
evanvd <ivanavdeev7347@gmail.com>
2026-02-09 21:19:45 +0300

    Первая версия

---
+++ DOSBox/Progi/GDDR/video.asm
@@ -0,0 +1,15 @@
+.model tiny
+.code
+org 100h
+VIDEOSEG equ 0B800h
+
+Start:    mov ax, VIDEOSEG
+		  mov es, ax
+		  
+		  mov al, ds:[82h]
+		  mov ah, 4eh
+		  xor bx, bx
+		  mov es :[bx], ax
+		  mov ax, 4c00h
+		  int 21h 
+end Start
