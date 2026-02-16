6bbed1bb642747dc2b63c785359a3dc538c6a04e
evanvd <ivanavdeev7347@gmail.com>
2026-02-16 16:11:23 +0300

    смещение

--- DOSBox/Progi/GDDR/video.asm
+++ DOSBox/Progi/GDDR/video.asm
@@ -5,11 +5,13 @@ VIDEOSEG equ 0B800h
 
 Start:    mov ax, VIDEOSEG
 		  mov es, ax
-		  
 		  mov al, ds:[82h]
 		  mov ah, 0CEh
-		  xor bx, bx
+		  mov bx, 0h
 		  mov es :[bx], ax
+		  
 		  mov ax, 4c00h
 		  int 21h 
+		  
 end Start
+
